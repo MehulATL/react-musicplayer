fb5f786f260c5c34cff68e21f338fe4cbf37c375
Mehul Patel <mehul8@gmail.com>
2016-08-05 11:15:49 -0700

    added routing/components for soundcloud callbacks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import configStore from './stores/configStore';
 import * as actions from './actions';
+import App from './components/App';
+import Callback from './components/Callback';
 import Stream from './components/Stream/';
 
 const tracks = [
@@ -16,10 +20,17 @@ const tracks = [
 
 const store = configStore();
 store.dispatch(actions.setTracks(tracks));
+const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Stream />
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Stream} />
+        <Route path="/" component={Stream} />
+        <Route path="/callback" component={Callback} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
